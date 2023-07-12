c3d8ea53b4cd1731acf2b51a4c1add8cfa452330
Fudgy-CL-SH-Code <iuploadgmod@gmail.com>
2023-07-12 01:23:35 -0400

    Added the default global enviorment variables
    a

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import Parser from "./frontend/parser.ts";
 import Env from "./runtime/env.ts";
 import { C_eval } from "./runtime/interp.ts";
+import { MK_BOOL, MK_NULL, MK_NUM } from "./runtime/values.ts";
 repl();
 
 function repl() {
@@ -9,6 +10,11 @@ function repl() {
   console.log("\nRepl v0.0.1");
   while (true) {
     const input = prompt(">> ");
+
+    // Create default Global Environment
+    env.declareVar("true", MK_BOOL(true), true);
+    env.declareVar("false", MK_BOOL(false), true);
+    env.declareVar("null", MK_NULL(), true);
 
     if (!input || input.includes("exit") || input.includes("quit")) {
       Deno.exit(1);
