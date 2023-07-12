83c43756bb9157a813f378e5f565585756138648
Fudgy-CL-SH-Code <iuploadgmod@gmail.com>
2023-07-12 12:02:08 -0400

    Updated README.md
    Filled out README

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# CookieLang
+CookieLang
+===
+- Authors:  `PrinceOfCookies`
+
+## Install & Dependence
+- deno
+
+## Use
+- for use
+  ```
+  deno run -A main.ts
+  ```
+
+
+## Directory Hierarchy
+```
+|—— .gitattributes
+|—— frontend
+|    |—— ast.ts
+|    |—— lexer.ts
+|    |—— parser.ts
+|—— main.ts
+|—— runtime
+|    |—— env.ts
+|    |—— eval
+|        |—— expressions.ts
+|        |—— statements.ts
+|    |—— interp.ts
+|    |—— values.ts
+```
+### Tested Platform
+- software
+  ```
+  OS: Windows 10
+  ```
+- hardware
+  ```
+  CPU: AMD Ryzen 5 1400 Quad-Core Processor
+  GPU: Nvidia GTX1050
+  ```
+## References
+- [Tutorial](https://github.com/tlaceby/guide-to-interpreters-series/tree/main)
+## License
+
+## Citing
+```
+```
--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
 import Parser from "./frontend/parser.ts";
 import Env from "./runtime/env.ts";
 import { C_eval } from "./runtime/interp.ts";
-import { MK_BOOL, MK_NULL, MK_NUM } from "./runtime/values.ts";
+import { MK_BOOL, MK_NULL } from "./runtime/values.ts";
 repl();
 
 function repl() {
   const parser = new Parser();
   const env = new Env();
   console.log("\nRepl v0.0.1");
+
+  // Create default Global Environment
+  env.declareVar("true", MK_BOOL(true), true);
+  env.declareVar("false", MK_BOOL(false), true);
+  env.declareVar("null", MK_NULL(), true);
+
   while (true) {
     const input = prompt(">> ");
-
-    // Create default Global Environment
-    env.declareVar("true", MK_BOOL(true), true);
-    env.declareVar("false", MK_BOOL(false), true);
-    env.declareVar("null", MK_NULL(), true);
 
     if (!input || input.includes("exit") || input.includes("quit")) {
       Deno.exit(1);
